f109b296a51d08c9c63fc9f734108eeb7b3b4e8f
msaikumar <sairam2854@gmail.com>
2025-05-15 15:16:37 +0530

    hello world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=vijju
 PERSON1=majju
 
-echo "$PERSON1":: hi $$PERSON2
+echo "$PERSON1":: hi $PERSON2
 echo "$PERSON2":: hi $PERSON1, how are you?
-echo "$PERSON1":: fine $$PERSON2, what about you? $$PERSON2
-echo "$$PERSON2":: same here also $PERSON1
+echo "$PERSON1":: fine $PERSON2, what about you? $PERSON2
+echo "$PERSON2":: same here also $PERSON1
