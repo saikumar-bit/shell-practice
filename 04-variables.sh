f3937d75aedd464f1be84eb7aaf7986a816138f4
msaikumar <sairam2854@gmail.com>
2025-05-15 15:26:45 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON1=$2
+
+echo "$PERSON1":: hi $PERSON2
+echo "$PERSON2":: hi $PERSON1, how are you?
+echo "$PERSON1":: fine $PERSON1, what about you? $PERSON1
+echo "$PERSON2":: same here also $PERSON2
