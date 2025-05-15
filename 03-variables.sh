e1e5f51349834fa4d6a78728db02ac1e25a39239
msaikumar <sairam2854@gmail.com>
2025-05-15 15:11:57 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=vijju
+PERSON1=majju
+
+echo "$PERSON=1":: hi $PERSON=2
+echo "$PERSON=2":: hi $PERSON=1, how are you?
+echo "$PERSON=1":: fine $PERSON=2, what about you? $PERSON=2
+echo "$PERSON=2":: same here also $PERSON=1
